f8b219bf89ff5c4f99c32e98d82eb8dc13ba1e9a
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2023-10-11 13:10:34 +0100

    feat:Utils done

--- src/net/salesianos/lacuesta/utils/Utils.java
+++ src/net/salesianos/lacuesta/utils/Utils.java
@@ -1,4 +1,35 @@
 package net.salesianos.lacuesta.utils;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class Utils {
-}
+    public class Utils {
+
+        public static ArrayList<String> getAllTextLineCounter(String fileRoute) {
+            ArrayList<String> TextLineCounter = new ArrayList<>();
+
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileRoute))) {
+                String currentLine = bufferedReader.readLine();
+
+                while (currentLine != null) {
+                    TextLineCounter.add(currentLine);
+                    currentLine = bufferedReader.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return TextLineCounter;
+        }
+
+        public static Integer getTotalVowelsFrom(String fileRoute) {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileRoute))) {
+                String firsLine = bufferedReader.readLine();
+                return Integer.parseInt(firsLine);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }
+    }
