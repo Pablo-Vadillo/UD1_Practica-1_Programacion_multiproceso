c16faff25e5894502f2d777eb17cf36cdb09d9cb
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2023-10-11 13:13:22 +0100

    feat:VowelLauncherDone

--- src/net/salesianos/lacuesta/launcher/VowelLauncher.java
+++ src/net/salesianos/lacuesta/launcher/VowelLauncher.java
@@ -1,4 +1,27 @@
 package net.salesianos.lacuesta.launcher;
 
+import java.io.File;
+import java.io.IOException;
+
 public class VowelLauncher {
+
+    private final static String OUTPUT_ROUTE = "src/net/salesianos/lacuesta/outputs/";
+
+    public static Process initVowelCounter(String text, String vowel, String fileName) {
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "java", "src/net/salesianos/lacuesta/processes/VowelCounter.java", text, vowel
+        );
+
+        Process javaProcess = null;
+
+        try {
+            File outputFile = new File(OUTPUT_ROUTE + fileName);
+            processBuilder.redirectOutput(outputFile);
+            javaProcess =  processBuilder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return javaProcess;
+    }
 }
+
