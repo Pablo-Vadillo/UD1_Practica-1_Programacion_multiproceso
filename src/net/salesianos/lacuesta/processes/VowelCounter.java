cd20abc7f125baeb83c0c73bef19ca04a5c46198
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2023-10-11 13:07:13 +0100

    feat:VowelCounter done

--- src/net/salesianos/lacuesta/processes/VowelCounter.java
+++ src/net/salesianos/lacuesta/processes/VowelCounter.java
@@ -1,4 +1,20 @@
 package net.salesianos.lacuesta.processes;
 
 public class VowelCounter {
+    public static void main(String[] args) {
+        String text = args[0];
+        String vowel = args[1];
+        int nCoincidencias = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+
+            String caracterComparar = String.valueOf(text.charAt(i));
+
+            if(caracterComparar.equals(vowel)){
+                nCoincidencias++;
+            }
+        }
+        System.out.println(nCoincidencias);
+    }
 }
+
